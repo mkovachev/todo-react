c98adbaa7d2fabee5f32cfa51cc74da52c260586
mkovachev <mkovachev6@gmail.com>
2019-12-23 13:43:46 +0200

    .

--- src/App.js
+++ src/App.js
@@ -5,9 +5,12 @@ function App() {
   const [todos, setTodos] = useState([])
   const todoNameRef = useRef()
   function handleAddTodo(e) {
-    const name = todoNameRef.current.value
+    const name = todoNameRef.current.value // create todo
     if (name === '') return
-    console.log(name)
+    setTodos(prevTodos => {
+      return [...prevTodos, { id: 1, name: name, complete: false }]
+    })
+    todoNameRef.current.value = null // clear input
   }
   return (
     <>

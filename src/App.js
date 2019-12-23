82510f8c8d68bf1d4bb40b21903068e577057302
mkovachev <mkovachev6@gmail.com>
2019-12-23 13:46:39 +0200

    install uuid

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React, { useState, useRef } from 'react'
 import TodoList from './TodoList'
+import uuidv4 from './uuidv4'
 
 function App() {
   const [todos, setTodos] = useState([])
   const todoNameRef = useRef()
   function handleAddTodo(e) {
-    const name = todoNameRef.current.value // create todo
+    const name = todoNameRef.current.value
     if (name === '') return
     setTodos(prevTodos => {
-      return [...prevTodos, { id: 1, name: name, complete: false }]
+      return [...prevTodos, { id: uuidv4(), name: name, complete: false }]
     })
-    todoNameRef.current.value = null // clear input
+    todoNameRef.current.value = null
   }
   return (
     <>
